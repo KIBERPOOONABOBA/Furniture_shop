ffbded480f1ece5e2234efbaf7ded60c1fec9aae
KIBERPOOONABOBA <vik.tor.bwvk@gmail/com>
2025-01-11 14:27:21 +0200

    products

---
+++ script.js
@@ -0,0 +1,30 @@
+let products = [
+    {
+      "title": "Парта растущая Pensare Grey",
+      "price": 22000,
+      "image": "https://img.mebelok.com/image/cache/data/FunDesk/23092021/parta_pensare_grey_foto_15-800x600.jpg.webp",
+       "description": "Парта растишка для школьника Pensare Grey Детский стол-трансформер Pensare Эргономичная, растущая парта с вместительной надстройкой. Надстройка включает в себя один выдвижной ящик, три полки, выдвижную подставку для книг, и специальные отверстия для крепления аксессуаров.",
+    },
+    {
+        "title": "Комплект парта и стул растущие Sole Grey",
+        "price": 3999,
+        "image": "https://img.mebelok.com/image/cache/data/FunDesk/aksessuary-dlya-detskoy/20240922-800x600.jpg.webp",
+        "description": "Sole предназначена для обеспечения более комфортной среды обучения. Стол и стул могут быть легко отрегулированы по высоте, что позволяет оптимально адаптировать его под соответствующий рост ребенка. ",
+      },
+      {
+        "title": "Диван угловой Диана ольха ",
+        "price": 35786,
+        "image": "https://img.mebelok.com/image/cache/data/Askalon/12024011-800x600.jpg.webp",
+        "description": "Диван угловой Диана – изысканный и интеллигентный. Он является отличным выбором для современного интерьера, выполненного в стиле модерн. Такая мебель станет изюминкой даже самого модного дизайна дома.  ",
+      }
+    ];
+function getCookieValue(cookieName) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(cookieName + 'm')) {
+            return cookie.substring(cookieName.lenght + 1);
+        }
+    }
+    return '';
+}
