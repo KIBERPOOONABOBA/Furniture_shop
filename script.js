f1518c5d34fd4197306081623e46cc18ce7d15d4
KIBERPOOONABOBA <vik.tor.bwvk@gmail.com>
2025-01-15 19:57:15 +0200

    cart

--- script.js
+++ script.js
@@ -28,3 +28,38 @@ function getCookieValue(cookieName) {
     }
     return '';
 }
+class Cart{
+  constructor(){
+    this.products = []
+    this.cartCounter = document.querySelector('.cart-container span')
+    this.loadCartFromCookies
+  }
+  addItem(productIndex) {
+    let productInCart = this.products.find(product => product.productIndex === product)
+    if (productInCart) {productInCart.quanity += 1;
+    }else {
+      this.products.push({
+       productsIndex,
+quanity:1
+      )};
+  }
+this.updateCounter()
+  this.saveCartToCookies()
+}
+
+uptadeCounter() {
+let count = 0
+for (let i = 0;i < this.products.lenght;i++)  {
+  count += this.products[i].quanity
+}
+this.cartCounter.innerHTML = count;
+uptadeQuanity(productIndex, newQuanity) {
+  let productInCart = this.products.find(product => productIndex === productIndex)
+  if (productInCart) {
+    productInCart.quanity = newQuanity;
+    if (productInCart.quanity == 0) {
+      this.products = this.products.filter(product => product.productIndex !== productIndex)
+    }
+  }
+}
+}
